ce2d846203a67d028607589a70945015993f4820
webmasterpf-DG <webmaster@provence-formation.com>
2016-04-15 12:12:59 +0200

    DEV : bug doublons et valeurs vides

--- node/node-poste_enseignement.tpl.php
+++ node/node-poste_enseignement.tpl.php
@@ -37,22 +37,33 @@
               
                foreach ($node->field_lieux as $key => $lycee) {
                    $liste_lycees = array_filter($lycee);//supprime les éléments vides
-                               print $liste_lycees['view'].'<br>';
-//           drupal_set_message(t("Clé : $key; Valeur lycee: $lycee<br />\n"));      
+                               print $liste_lycees['view'].'<br>';//Affiche les termes sans doublons
+               
+             
 //Récupération du tid dans une variable, suppression des doublon                               
-                               foreach ($lycee as $key => $term_tid) {
-                   //drupal_set_message(t("Clé : $key; Valeur term id: $term_tid<br />\n"));      
-//drupal_set_message(t("Clé : $key; Valeur lycee: $lycee<br />\n"));     
-                                     $liste_lieux .= $lycee['value'].',';
-                                   if (!in_array($lycee,$liste_lieux))
+                               foreach ($lycee as $keytid => $term_tid) {
+                                                 $tab_liste_lieux[] .= $lycee['value'];
+                  $tab_liste_lieux = array_unique($tab_liste_lieux); //suppression des doublons       
+                  
+                 
+                 
+              
    {
-                                       $tab_liste_lieux[] .= $lycee['value'].'|';
-//         drupal_set_message(t("Clé : $key; Valeur liste lieux: $liste_lieux<br />\n"));                                         
+                                                  
+                      
    }                                    
      
                                }
-                  
+               
                }
+                 foreach ($tab_liste_lieux as $keylieux => $liste_lieux) {
+//                 drupal_set_message(print_r($tab_liste_lieux, TRUE));           
+                    $liste_lieux .= $tab_liste_lieux['value'].','; 
+                }
+//               drupal_set_message(print_r($liste_lycees, TRUE)); 
+                drupal_set_message(t("Clé tableau lycée : $key; Valeur tableau lycee: $lycee<br />\n")); 
+                drupal_set_message(t("Valeur var liste_lieux: $liste_lieux<br />\n")); 
+                drupal_set_message(print_r($tab_liste_lieux, TRUE)); 
            ?></td>
 
            </tr>
